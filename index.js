3a9354f089012a8fad5c513e1e9de44ddadff118
Ponhagen <pontus.havmyr@stud.hkr.se>
2025-04-16 15:03:44 +0200

    Connection to MongoDB

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ const connectionString = process.env.CONNECTION_URL;
 
 // Middleware to read JSON-data
 app.use(express.json());
+
+
+
+
+
+
+
+// Connection to MongoDB
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Ansluten till MongoDB");
+    app.listen(port, () => {
+      console.log(`Servern körs på http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Fel vid anslutning till databasen:", err.message);
+  });
