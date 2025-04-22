424332da100ced4a5f3e664840fb8a7535d13e71
Ponhagen <pontus.havmyr@stud.hkr.se>
2025-04-22 16:15:04 +0200

    Update for index.html and script.js

--- public/script.js
+++ public/script.js
@@ -13,18 +13,17 @@ async function getDish() {
     data.forEach(dish => {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${dish.name}</td>
-            <td>${dish.Ingredients.join(',')}</td>
-            <td>${dish.preperationSteps.join(',')}</td>
-            <td>${dish.cookingTime} min</td>
-            <td>${dish.difficulty}</td>
-
-            <td>
-            <td><button onclick="delete(${dish.id})">Delete</button></td>
-            <td><button onclick="update(${dish.id})">Update</button></td>
-            <td>
-
-        `;
+        <td>${dish.name}</td>
+        <td>${dish.ingredients.join(', ')}</td>
+        <td>${dish.preperationSteps.join(', ')}</td>
+        <td>${dish.cookingTime} min</td>
+        <td>${dish.difficulty}</td>
+        <td>
+          <button onclick="deleteDish('${dish._id}')">Delete</button>
+          <button onclick="updateDish('${dish._id}')">Update</button>
+        </td>
+      `;
+      
         tbody.appendChild(row);
     });
 }
